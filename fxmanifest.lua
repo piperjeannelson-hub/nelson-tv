834002107560dadc160c10a1c303d84bccb092b1
gamingwithmaddie <piperjeannelson@gmail.com>
2026-06-04 14:04:42 -0400

    Add resource manifest file

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'TV Screen Script - Play videos on props with screens'
+version '1.0.0'
+
+scripts {
+    'client.lua',
+    'server.lua'
+}
+
+dependencies {
+    '/server:4960'
+}
